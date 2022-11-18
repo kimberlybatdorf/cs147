dcb368e8947511523e1345e57b0a5671cc0e829b
Sharon Wambu <sharonwambu@DN0a22c5e5.SUNet>
2022-11-18 13:43:46 -0800

    set up packages for expo go

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View, Text, Image, FlatList, List } from "react-native";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+
+
+export default function App() {
+
+
+
+}
