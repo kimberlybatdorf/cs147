297bafe9bebeb2521733aa088852e40a5cb2e36e
Sharon Wambu <sharonwambu@DN0a22c5e5.SUNet>
2022-11-18 13:53:55 -0800

    debugged blank app

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
-import { StyleSheet, View, Text, Image, FlatList, List } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text, Image, FlatList, List } from "react-native";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 
 export default function App() {
-
+    return(
+        <SafeAreaView style={styles.container}>
+            {/* TODO: Your code goes here */}
+            <Text>check check one two three</Text>
+            
+          </SafeAreaView>
+        );
 
 
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: "center",
+      alignItems: "center",
+      flex: 1,
+    },
+    button: {
+      borderRadius: 99999,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    buttonImage:{
+      resizeMode: "contain",
+      height: 50,
+      width: 50,
+    },
+    
+  
+  });
