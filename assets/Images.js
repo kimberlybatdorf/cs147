2e4a6c77d56a615d1f045026cec2ef2fae787b17
Sharon Wambu <sharonwambu@Sharons-MBP-3.lan>
2022-11-20 13:50:13 -0800

    started on images.js file so i can easily get the icon i want in app.js

---
+++ assets/Images.js
@@ -0,0 +1,9 @@
+const images = {
+    spotify: require('../spotify-logo.png'),
+    analyticsOutline: require('../analyticsicon.png'),
+    analyticsFilled: require('../analyticsicon2.png'),
+    backArrow: require('../communityicon.png'),
+    filterbutton: require('../')
+  };
+  
+  export default images;
