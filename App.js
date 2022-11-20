8086caea4b41ab70561a31b5844bccdd05d418b8
Sharon Wambu <sharonwambu@Sharons-MBP-3.lan>
2022-11-20 13:15:52 -0800

    downloaded bottom tabs package, continued working on navigation (buggy at the moment)

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StyleSheet, SafeAreaView, View, Text, Image, FlatList, List } from "rea
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons';
 
 
-export default function App() {
+
 
     function UpdatesScreen() {
         return (
@@ -32,14 +33,12 @@ export default function App() {
       }
 
       const Tab = createBottomTabNavigator();
-
+export default function App() {
 
       //Starting on navigation, will look at this chunk of code later
 //       <NavigationContainer>
 //       <Tab.Navigator>
-//       tabBarOptions={{
-//     labelStyle: { fontSize: 14 },
-//   }}
+//       
 //    screenOptions={({ route }) => ({
 //           tabBarIcon: ({ focused }) => {
 //           }
@@ -52,12 +51,29 @@ export default function App() {
     return(
         <NavigationContainer>
             <Tab.Navigator>
-                
-        <SafeAreaView style={styles.container}>
-            {/* TODO: Your code goes here */}
-            <Text>check check one two three</Text>
+            screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            let iconSource;
+
+            if (route.name === 'Updates') {
+              iconSource = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Analytics') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            } else if (route.name === 'Community') {
+              iconName = focused
+                ? 'information-circle'
+                : 'information-circle-outline';
+            }
+
+            return <Ionicons name={iconName} size={24} color="black" />;
+          }
+        })}>
             
-          </SafeAreaView>
+            
+
+        <Tab.Screen name="Updates" component={UpdatesScreen} />
+        <Tab.Screen name="Analytics" component={AnalyticsScreen} />
+        <Tab.Screen name="Community" component={CommunityScreen} />
           </Tab.Navigator>
           </NavigationContainer>
         );
