ef470672e9f527d8a9c1482035a018099dc6aeae
kimberlybatdorf <kbatdorf@stanford.edu>
2022-11-29 12:22:13 -0800

    add more styling

--- UpdatesTab.js
+++ UpdatesTab.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, View, Text, TextInput, Image, FlatList, List, ImageBackgroundBase } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text, TextInput, Image, FlatList, List, ImageBackgroundBase, Button } from "react-native";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
@@ -7,6 +7,7 @@ import images from "./assets/Images";
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 function UpdatesHomeScreen() {
@@ -37,45 +38,75 @@ function UpdatesHomeScreen() {
     return (
       <SafeAreaView style={styles.container}>
         {/* BACK ARROW BUTTON */}
-        <Text style={styles.screenText}>DATE</Text>
-        <Text style={styles.screenText}>Creating New Post</Text>
-        {/* <Image source={images.updatesFilled}/> */}
-        <Text style={styles.screenText}>Symptoms</Text>
+        <Text style={{alignSelf: 'center', fontSize: 42, color: '#FFFFFF', marginTop: 12}}>DATE</Text>
+        <Text style={{fontSize: 32, color: '#FFFFFF', padding: 15}}>Symptoms</Text>
+        <View  
+          style={{
+            flexDirection: "row",
+            height: 100,
+            padding: 15,
+            justifyContent: 'space-evenly'
+        }}>
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+        </View>
+        <View  
+          style={{
+            flexDirection: "row",
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            paddingLeft: 30,
+            paddingRight: 30
+        }}>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Fatigue</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Nausea</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Dizziness</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Headache</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Eye strain</Text>
+        </View>
+        <Text style={{fontSize: 32, color: '#FFFFFF', padding: 15}}>Mood</Text>
         <View  
           style={{
             flexDirection: "row",
             height: 100,
             padding: 20
         }}>
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
+          <BouncyCheckbox size={60} fillColor='green' onPress={(isChecked) => {}} />
         </View>
-        <Text style={styles.screenText}>Mood</Text>
         <View  
           style={{
             flexDirection: "row",
-            height: 100,
-            padding: 20
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            paddingLeft: 30,
+            paddingRight: 30
         }}>
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
-          <BouncyCheckbox onPress={(isChecked) => {}} />
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Happy</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Stressed</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Satisfied</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Isolated</Text>
+          <Text style={{fontSize: 12, color: '#FFFFFF'}}>Hopeful</Text>
         </View>
-        <Text style={styles.screenText}>Journal</Text>
+        <Text style={{fontSize: 32, color: '#FFFFFF', padding: 15}}>Journal</Text>
         <TextInput
-          style={{height: 40, backgroundColor: '#313033', color: '#FFFFFF'}}
+          style={{borderWidth: 12, height: 150, margin: 15, borderRadius: 15, borderColor: '#313033', backgroundColor: '#313033', color: '#FFFFFF', fontSize: 18}}
           placeholder="How are you feeling today? In what ways do you need support? What have you accomplished today?"
           placeholderTextColor='#E5E1E5'
           onChangeText={newText => setText(newText)}
           defaultValue={text}
+          multiline={true}
         />
+        <TouchableOpacity style={styles.saveButton}> 
+          <Text style={styles.saveText}>Save</Text>
+        </TouchableOpacity>
         {/* MICROPHONE ICON */}
-        {/* SAVE BUTTON */}
       </SafeAreaView>
     );
   }
@@ -122,9 +153,10 @@ export default function UpdatesTab(){
 
 const styles = StyleSheet.create({
     container: {
-      justifyContent: "center",
-      alignItems: "center",
+      justifyContent: "flex-start",
+      // alignItems: "flex-start",
       flex: 1,
+      backgroundColor: '#464646'
     },
     button: {
       borderRadius: 99999,
@@ -139,6 +171,20 @@ const styles = StyleSheet.create({
     },
     screenText: {
         fontSize: 32,
-      }
+        color: '#FFFFFF'
+    },
+    saveButton:{
+      marginRight:40,
+      marginLeft:40,
+      marginTop:10,
+      paddingTop:10,
+      paddingBottom:10,
+      backgroundColor:'green',
+      borderRadius:15,
+    },
+    saveText:{
+        color:'#ffffff',
+        textAlign:'center',
+    }
   
   });
